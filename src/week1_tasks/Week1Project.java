1558b0636652b8a69677e3f205c7dc84b37d9c29
Rodri <Rodriguezcr2@gmail.com>
2023-05-22 22:29:54 -0400

    Updated week1 task 1

--- src/week1_tasks/Week1Project.java
+++ src/week1_tasks/Week1Project.java
@@ -2,5 +2,24 @@ package week1_tasks;
 
 public class Week1Project {
 
-    // Please start project for week 1
+    public static void main(String[] args) {
+
+        int num1 = 5;
+        int num2 = 6;
+
+        boolean isEven = num1 % 2 == 0; // When we divide a number by 2, if the remainder is 0, mean the number is even
+
+        // When we divide a number by 2, if the remainder is NOT 0, mean the number is odd
+
+        boolean isOdd = !isEven; //If the number is not even, then is odd
+
+        System.out.println(num1 + " is an evan number: " + isEven );
+        System.out.println(num2 + " is an odd number: " + isOdd);
+
+
+
+    }
+
+
 }
+
