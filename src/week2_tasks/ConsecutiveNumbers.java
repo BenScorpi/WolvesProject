95ab5f28c176577b2a3986c9d0ab48906fd99573
Elif_Cydeo <elif.demirbass10@gmail.com>
2023-05-24 21:11:21 -0400

    created Consecutive Numbers

---
+++ src/week2_tasks/ConsecutiveNumbers.java
@@ -0,0 +1,23 @@
+package week2_tasks;
+
+public class ConsecutiveNumbers {
+    public static void consecutiveNumbers(int N) {
+        for (int i = 1; i <= N; i++) {
+            if (i % 2 == 0 && i % 3 == 0 && i % 5 == 0) {
+                System.out.println("CodilityTestCoders");
+            } else if (i % 2 == 0 && i % 3 == 0) {
+                System.out.println("CodilityTest");
+            } else if (i % 2 == 0) {
+                System.out.println("Codility");
+            } else if (i % 3 == 0) {
+                System.out.println("Test");
+            } else if (i % 5 == 0) {
+                System.out.println("Coders");
+            } else {
+                System.out.println(i);
+            }
+
+        }
+
+    }
+}
