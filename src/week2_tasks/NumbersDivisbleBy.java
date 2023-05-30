7d824ace6c948ff87a456d107979121feb0fabca
Rodri <Rodriguezcr2@gmail.com>
2023-05-30 00:21:04 -0400

    Updated code for divisible numbers.

--- src/week2_tasks/NumbersDivisbleBy.java
+++ src/week2_tasks/NumbersDivisbleBy.java
@@ -3,18 +3,23 @@ package week2_tasks;
 public class NumbersDivisbleBy {
     public static void main(String[] args) {
 
+        //declaring the varibale
+        int number = 15;
+        //setting condtion of variable
+        for (int i = 1; i <= 100; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
 
-        int number = 900;
+            }
+            boolean isDivisibleBy3 = number % 3 == 0;        // if the remainder of the number divided by 3 is equal to zero, then it's evenly divisible by 3
+            boolean isDivisibleBy5 = number % 5 == 0;        // if the remainder of the number divided by 5 is equal to zero, then it's evenly divisible by 5
+            boolean isDivisibleBy15 = number % 15 == 0;        // if the remainder of the number divided by 15 is equal to zero, then it's evenly divisible by 15
 
-        boolean isDivisibleBy3 =  number % 3 == 0;        // if the remainder of the number divided by 3 is equal to zero, then it's evenly divisible by 3
-        boolean isDivisibleBy5 = number % 5 == 0;        // if the remainder of the number divided by 5 is equal to zero, then it's evenly divisible by 5
-        boolean isDivisibleBy15 = number % 15 == 0;        // if the remainder of the number divided by 15 is equal to zero, then it's evenly divisible by 15
+            System.out.println(number + " is divisible by 3: " + isDivisibleBy3);
+            System.out.println(number + " is divisible by 5: " + isDivisibleBy5);
+            System.out.println(number + " is divisible by 15: " + isDivisibleBy15);
 
-        System.out.println(number + " is divisible by 3: " + isDivisibleBy3);
-        System.out.println(number + " is divisible by 5: " + isDivisibleBy5);
-        System.out.println(number + " is divisible by 15: " + isDivisibleBy15);
+        }
 
     }
 
 }
-
