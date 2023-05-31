26da8e752ae39a21605707fda6321553e47bfa9e
Elif_Cydeo <elif.demirbass10@gmail.com>
2023-05-31 08:23:54 -0400

    updated Consecutive Numbers

--- src/week2_tasks/ConsecutiveNumbers.java
+++ src/week2_tasks/ConsecutiveNumbers.java
@@ -3,21 +3,34 @@ package week2_tasks;
 public class ConsecutiveNumbers {
     public static void consecutiveNumbers(int N) {
         for (int i = 1; i <= N; i++) {
-            if (i % 2 == 0 && i % 3 == 0 && i % 5 == 0) {
-                System.out.println("CodilityTestCoders");
-            } else if (i % 2 == 0 && i % 3 == 0) {
-                System.out.println("CodilityTest");
-            } else if (i % 2 == 0) {
-                System.out.println("Codility");
-            } else if (i % 3 == 0) {
-                System.out.println("Test");
-            } else if (i % 5 == 0) {
-                System.out.println("Coders");
+            boolean divisible = false;
+            StringBuilder result = new StringBuilder();
+
+            if (i % 2 == 0) {
+                result.append("Codility");
+                divisible = true;
+            }
+            if (i % 3 == 0) {
+                result.append("Test");
+                divisible = true;
+            }
+            if (i % 5 == 0) {
+                result.append("Coders");
+                divisible = true;
+            }
+
+            if (divisible) {
+                System.out.println(result);
             } else {
                 System.out.println(i);
             }
-
         }
-
     }
+
+    public static void main(String[] args) {
+        consecutiveNumbers(30);
+    }
+
 }
+
+
