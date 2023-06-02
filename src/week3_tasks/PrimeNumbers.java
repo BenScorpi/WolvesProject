436383c86511ce0f5aa9a2494a91940f33cf2bcf
MakbuleB <enderbahar@gmail.com>
2023-06-02 14:46:34 -0400

    Prime Number Task is done

---
+++ src/week3_tasks/PrimeNumbers.java
@@ -0,0 +1,22 @@
+package week3_tasks;
+
+public class PrimeNumbers {
+
+    public static void main(String[] args) {
+
+        System.out.println(isPrime(5));
+
+    }
+
+
+    public static boolean isPrime(int n)  {
+
+        for(int i=2;i<=n/2;i++)
+            if(n%i==0){
+                return false;
+            }
+
+                return true;
+    }
+        }
+
