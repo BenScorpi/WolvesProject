769868c3805e8184f619d15144a43b11b6a87e48
MakbuleB <enderbahar@gmail.com>
2023-06-07 09:42:58 -0400

    Prime Numbers Task is updated

--- src/week3_tasks/PrimeNumbers.java
+++ src/week3_tasks/PrimeNumbers.java
@@ -4,14 +4,14 @@ public class PrimeNumbers {
 
     public static void main(String[] args) {
 
-        System.out.println(isPrime(5));
+        System.out.println(isPrime(8));
 
     }
 
 
     public static boolean isPrime(int n)  {
 
-        for(int i=2;i<=n/2;i++)
+        for(int i=2;i<=Math.sqrt(n);i++)
             if(n%i==0){
                 return false;
             }
