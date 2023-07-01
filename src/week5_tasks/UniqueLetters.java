e1d08d57cbba039d6f2552ed72b76b57b83a4e5d
Elif_Cydeo <elif.demirbass10@gmail.com>
2023-07-01 18:51:11 -0400

    UniqueLetters added

---
+++ src/week5_tasks/UniqueLetters.java
@@ -0,0 +1,30 @@
+package week5_tasks;
+
+public class UniqueLetters {
+
+    //String -- Find the unique
+    //Write a return method that can find the unique characters from the String
+    //Ex: unique("AAABBBCCCDEF") ==> "DEF";
+
+    public static String uniqueLetters(String str){
+
+        String result = "";
+
+        for (int i = 0; i < str.length(); i++) {
+            int count = 0;
+            for (int j = 0; j < str.length(); j++) {
+                if(str.charAt(i) == str.charAt(j)){
+                    count++;
+                }
+            }
+            if(count ==1){
+                result+= str.charAt(i);
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(uniqueLetters("AAABBBCCCDEF"));
+    }
+}
