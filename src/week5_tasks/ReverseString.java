d905e5f80ee5e9ba8b1766db5d5c9be097b6451d
Elif_Cydeo <elif.demirbass10@gmail.com>
2023-07-01 18:50:48 -0400

    ReverseString added

---
+++ src/week5_tasks/ReverseString.java
@@ -0,0 +1,22 @@
+package week5_tasks;
+
+public class ReverseString {
+
+    //String -- Reverse
+    //Write a return method that can reverse String
+    //Ex: Reverse("ABCD"); ==> DCBA
+
+    public static String reverseString(String str){
+        String result = "";
+
+        for (int i = str.length()-1; i >=0 ; i--) {
+            result += str.charAt(i);
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(reverseString("ABCD"));
+    }
+}
